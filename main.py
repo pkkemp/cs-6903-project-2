ed746e63f01128936f2efa389b6f9ebd649e71be
Smita Jain <sj1525@nyu.edu>
2021-04-28 18:25:41 -0700

    Typing animation added, bugs fixed

--- main.py
+++ main.py
@@ -2,7 +2,12 @@
 
 import operator
 from operator import itemgetter
+import time 
 
+def typer(strToPrint):
+    for char in strToPrint:
+        print(char, end='',flush=True)
+        time.sleep(.03)        
 def main():
 
 # higher efficiency is faster
@@ -81,14 +86,17 @@ def main():
     q4 = '\nIs encrypted information being exposed a concern for you?'
     q5 = '\nIs speed a concern for you?'
 
-    
-    print('\n\nWelcome! This program is meant to help you figure out which partially homomorphic encryption scheme is best for your use case.')
-    print('\nThere will be around 4-5 multiple choice questions for you to answer. You can provide your answers by entering in numbers like 1 or 2 into the command line. \n\n')
-    print(q1)
+    IntroStr1 = '\n\nWelcome! This program is meant to help you figure out which partially homomorphic encryption scheme is best for your use case.'
+    IntroStr2 = '\nThere will be around 4-5 multiple choice questions for you to answer. You can provide your answers by entering in numbers like 1 or 2 into the command line. \n\n'
+   
+    typer(IntroStr1)
+    typer(IntroStr2)
+
+    typer(q1)
     a1 = input('\nEnter one of the options below: \n\n 1 - Yes \n 2 - No \n\n')
 
     if a1 == '1':
-        print(q2)
+        typer(q2)
         a2 = input('\nEnter one of the options below: \n\n 1 - Election/Voting \n 2 - Electronic Cash \n 3 - Calculating Averages \n 4 - Calculating Probabilities \n 5 - Checking Data Integrity \n 6 - Creating Digital Signatures \n 7 - Searching Over a Filesystem / Database \n 8 - Other \n\n')
        	if a2 == '1':
        		options.append(paillier)
@@ -108,10 +116,10 @@ def main():
        	    options.append(el_gamal)
        	    options.append(rabin)
        	elif a2 == '7':
-       	    print('\nYou should consider a fully homomorphic encryption system. That should provide more flexibility in what you are able to compute over your encrypted data.\n\nTerminating program, goodbye!\n\n')
+       	    typer('\nYou should consider a fully homomorphic encryption system. That should provide more flexibility in what you are able to compute over your encrypted data.\n\nTerminating program, goodbye!\n\n')
        	    exit()
        	elif a2 == '8':
-       	    print(q3)
+       	    typer(q3)
        	    a3 = input('\nChoose the best fit of the options below: \n\n 1 - Addition, Multiplication by an integer, and/or Exponentiation by an integer \n 2 - Multiplication, Division, and/or Exponentiation \n 3 - Parity, Complement \n 4 - Linear Regression \n\n')
        	    if a3 == '1':
        	        options.append(paillier)
@@ -128,35 +136,38 @@ def main():
                 options.append(paillier)
        	        options.append(damgard_jurik)
        	    else:
-       	        print('\nYour input is invalid.\n\nTerminating program, goodbye!\n\n')
+       	        typer('\nYour input is invalid.\n\nTerminating program, goodbye!\n\n')
+                return
        	else:
-       	    print('\nYour input is invalid.\n\nTerminating program, goodbye!\n\n')
-        print(q4)
+       	    typer('\nYour input is invalid.\n\nTerminating program, goodbye!\n\n')
+            return
+        typer(q4)
         a4 = input('\nEnter one of the options below: \n\n 1 - Yes \n 2 - No \n\n')
         if a4 == '1':
             if textbook_rsa in options: options.remove(textbook_rsa)
         elif a4 == '2':
             pass
         else:
-            print('\nYour input is invalid.\n\nTerminating program, goodbye!\n\n')
-        print(q5) 
+            typer('\nYour input is invalid.\n\nTerminating program, goodbye!\n\n')
+        typer(q5) 
         a5 = input('\nEnter one of the options below: \n\n 1 - Yes \n 2 - No \n\n')
         if a5 == '1':
             options = sorted(options, key = itemgetter('efficiency'), reverse=True)
         elif a5 == '2':
             pass
         else:
-            print('\nYour input is invalid.\n\nTerminating program, goodbye!\n\n')
+            typer('\nYour input is invalid.\n\nTerminating program, goodbye!\n\n')
         final_answer = options[0]
         text = '\nWe recommend that the partially homomorphic encryption scheme you use is the {} cryptosystem.\n'
-        print(text.format(final_answer['name']))
+        typer(text.format(final_answer['name']))
         text2 = '\nHere\'s some more details about this scheme: \n - Its cryptographic security level is {}. \n - Its hardness assumption is the {} problem. \n - Its homomorphisms are {}, and its malleability properties are {}. \n - Its efficiency, on our scale from 1-5, is {}. \n\n'
-        print(text2.format(final_answer['security'], final_answer['hardness'], final_answer['homomorphism'], final_answer['malleability'], final_answer['efficiency']))
-        print('\nThanks for playing! We hope this was useful. \n\nTerminating program, goodbye!\n\n')
+        typer(text2.format(final_answer['security'], final_answer['hardness'], final_answer['homomorphism'], final_answer['malleability'], final_answer['efficiency']))
+        typer('\nThanks for playing! We hope this was useful. \n\nTerminating program, goodbye!\n\n')
     elif a1 == '2':
-        print('\nYou should consider a fully homomorphic encryption system. That should provide more flexibility in what you are able to compute over your encrypted data.\n\nTerminating program, goodbye!\n\n')
+        typer('\nYou should consider a fully homomorphic encryption system. That should provide more flexibility in what you are able to compute over your encrypted data.\n\nTerminating program, goodbye!\n\n')
     else:
-        print('\nYour input is invalid.\n\nTerminating program, goodbye!\n\n')
+        typer('\nYour input is invalid.\n\nTerminating program, goodbye!\n\n')
+        return
 
 
 if __name__ == '__main__':
